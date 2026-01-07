83d3c23c496bd2ac61524e7da8e96b809a91a219
GelerCAT <gelercat117@gmail.com>
2026-01-07 14:30:47 +0800

    add relay

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MLTeamWork-Buaa2025Fall
 buaa 2025 团队作业
+# install
+`uv sync`
+# Dataset
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mlteamwork-buaa2025fall!")
+
+
+if __name__ == "__main__":
+    main()
